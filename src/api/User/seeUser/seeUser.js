d760a947323c490c54c860caba8413bf98c50d1f
dave <daveg7lee@gmail.com>
2020-12-06 15:16:25 +0900

    Kill the fragments

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -4,12 +4,7 @@ export default {
   Query: {
     seeUser: async (_, args) => {
       const { id } = args;
-      const user = await prisma.user({ id });
-      const posts = await prisma.user({ id: user.id }).posts();
-      return {
-        user,
-        posts,
-      };
+      return prisma.user({ id });
     },
   },
 };
